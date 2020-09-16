05d26895c78402f92070bb468239bdc2fcdf065c
iffazainannisa <iffazainannisa@student.undip.ac.id>
2020-09-16 14:48:21 +0700

    Initial Commit

---
+++ asset/function.js
@@ -0,0 +1,10 @@
+
+// Responsive Navbar
+function myNavbar() {
+  var x = document.getElementById("myMenu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
